9d4e5d914fab60eade70b8be95d89535b3687255
nicolas.bury <nicolas.bury@tpgtechnology.com>
2022-03-29 21:48:14 -0400

    added main.go

---
+++ main.go
@@ -0,0 +1,129 @@
+package main
+
+import (
+  "fmt"
+  "strings"
+  "net/http"
+  "io/ioutil"
+)
+
+type AccountData struct {
+	Attributes     *AccountAttributes `json:"attributes,omitempty"`
+	ID             string             `json:"id,omitempty"`
+	OrganisationID string             `json:"organisation_id,omitempty"`
+	Type           string             `json:"type,omitempty"`
+	Version        *int64             `json:"version,omitempty"`
+}
+
+type AccountAttributes struct {
+	AccountClassification   *string  `json:"account_classification,omitempty"`
+	AccountMatchingOptOut   *bool    `json:"account_matching_opt_out,omitempty"`
+	AccountNumber           string   `json:"account_number,omitempty"`
+	AlternativeNames        []string `json:"alternative_names,omitempty"`
+	BankID                  string   `json:"bank_id,omitempty"`
+	BankIDCode              string   `json:"bank_id_code,omitempty"`
+	BaseCurrency            string   `json:"base_currency,omitempty"`
+	Bic                     string   `json:"bic,omitempty"`
+	Country                 *string  `json:"country,omitempty"`
+	Iban                    string   `json:"iban,omitempty"`
+	JointAccount            *bool    `json:"joint_account,omitempty"`
+	Name                    []string `json:"name,omitempty"`
+	SecondaryIdentification string   `json:"secondary_identification,omitempty"`
+	Status                  *string  `json:"status,omitempty"`
+	Switched                *bool    `json:"switched,omitempty"`
+}
+
+func main() {
+  fetchAccounts()
+  createAccount()
+}
+
+func fetchAccounts(){
+	url := "http://localhost:8080/v1/organisation/accounts"
+  method := "GET"
+
+  client := &http.Client {
+  }
+  req, err := http.NewRequest(method, url, nil)
+
+  if err != nil {
+    fmt.Println(err)
+    return
+  }
+  
+  res, err := client.Do(req)
+  if err != nil {
+    fmt.Println(err)
+    return
+  }
+  defer res.Body.Close()
+
+  body, err := ioutil.ReadAll(res.Body)
+  if err != nil {
+    fmt.Println(err)
+    return
+  }
+  fmt.Println(string(body))
+}
+
+func createAccount() {
+
+  url := "http://localhost:8080/v1/organisation/accounts"
+  method := "POST"
+  var account AccountData
+  var attributes AccountAttributes
+
+  attributes.Country = "GB"
+  attributes.BaseCurrency = "GBP"
+
+  account.ID = "2f4dd848-afc9-11ec-b909-0242ac120002"
+  account.OrganisationID = "3161a9d0-afc3-11ec-b909-0242ac120002"
+  account.Type = "accounts"
+
+
+  json := `{
+  "data": {
+    "id": "3161a6a6-afc3-11ec-b909-0242ac120002",
+    "organisation_id": "3161a9d0-afc3-11ec-b909-0242ac120002",
+    "type": "accounts",
+    "attributes": {
+       "country": "GB",
+        "base_currency": "GBP",
+        "bank_id": "400302",
+        "bank_id_code": "GBDSC",
+        "account_number": "10000004",
+        "customer_id": "234",
+        "iban": "GB28NWBK40030212764204",
+        "bic": "NWBKGB42",
+        "name": "{Nick, Bury}",
+        "account_classification": "Personal"
+    }
+  }
+}`
+
+  
+  payload := strings.NewReader(json)
+
+  client := &http.Client {
+  }
+  req, err := http.NewRequest(method, url, payload)
+
+  if err != nil {
+    fmt.Println(err)
+    return
+  }
+
+  res, err := client.Do(req)
+  if err != nil {
+    fmt.Println(err)
+    return
+  }
+  defer res.Body.Close()
+
+  body, err := ioutil.ReadAll(res.Body)
+  if err != nil {
+    fmt.Println(err)
+    return
+  }
+  fmt.Println(string(body))
+}
